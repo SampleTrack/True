6f72f86bc143ac0f0622ed04af3840207e38d959
SampleTrack <FirstSample@proton.me>
2026-05-03 22:59:57 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -9,11 +9,10 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
 from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER
 
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-saveMedia = None
+
 client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
 instance = Instance.from_db(db)

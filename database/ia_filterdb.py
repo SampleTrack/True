b264ffaaf3055a3aaaf57cc25f2a8e935f082015
SampleTrack <FirstSample@proton.me>
2026-05-03 15:08:33 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -13,7 +13,6 @@ from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTE
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-#some basic variables needed
 saveMedia = None
 client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]

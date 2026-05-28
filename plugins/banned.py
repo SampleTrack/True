142a63601fd9660b69abfa2ca5286436a04a6bac
SampleTrack <FirstSample@proton.me>
2026-05-28 07:01:37 +0530

    fix: banned_users filter AttributeError when from_user is None

--- plugins/banned.py
+++ plugins/banned.py
@@ -5,17 +5,17 @@ from database.users_chats_db import db
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import SUPPORT_CHAT
 
+# FIX: original logic caused AttributeError when from_user is None
+# Simplified to: only check banned if there's an actual user
 async def banned_users(_, client, message: Message):
-    return (
-        message.from_user is not None or not message.sender_chat
-    ) and message.from_user.id in temp.BANNED_USERS
+    return message.from_user is not None and message.from_user.id in temp.BANNED_USERS
 
 banned_user = filters.create(banned_users)
 
 async def disabled_chat(_, client, message: Message):
     return message.chat.id in temp.BANNED_CHATS
 
-disabled_group=filters.create(disabled_chat)
+disabled_group = filters.create(disabled_chat)
 
 
 @Client.on_message(filters.private & banned_user & filters.incoming)
@@ -28,7 +28,7 @@ async def grp_bd(bot, message):
     buttons = [[
         InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')
     ]]
-    reply_markup=InlineKeyboardMarkup(buttons)
+    reply_markup = InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
     k = await message.reply(
         text=f"CHAT NOT ALLOWED 🐞\n\nMy admins has restricted me from working here ! If you want to know more about it contact support..\nReason : <code>{vazha['reason']}</code>.",
